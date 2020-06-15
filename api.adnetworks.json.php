0b967bdd558c72c6da08e1d5588a06dd50f74dc0
Namhyeon, Go <gnh1201@gmail.com>
2020-06-15 11:13:14 +0900

    Update api.adnetworks.json.php

--- api.adnetworks.json.php
+++ api.adnetworks.json.php
@@ -20,6 +20,7 @@ if(empty($limit)) {
 
 $tablename = exec_db_table_create(array(
 	"hash" => array("varchar", 255),
+	"type" => array("varchar", 45),
 	"datetime" => array("datetime"),
 	"title" => array("varchar", 255),
 	"category" => array("varchar", 255),
@@ -29,75 +30,77 @@ $tablename = exec_db_table_create(array(
 	"discount_price" => array("int", 20),
 	"network" => array("varchar", 255)
 ), "adnetwork_items", array(
-	"setindex" => array(
-		"index_1" => array("hash"),
-		"index_2" => array("datetime")
-	)
+    "setindex" => array(
+        "index_1" => array("hash"),
+        "index_2" => array("datetime")
+    )
 ));
 
 // tenping
 $response = get_web_json("http://tenping.kr/adbox/list", "get", array(
-	"MemberID" => "qa72oCvud/nRRFb92UdVzDZIu0SUT0v2kfQMkS0t+MiMbkZAw9D+O5BCCflZXTru",
-	"PageSize" => 10,
-	"CampaignType" => 0,
-	"MinClickPoint" => 0,
-	"MinCurrentPoint" => 0,
-	"ExistMiddleImage" => "",
-	"ExistRectangleImage" => "",
-	"IsPartner" => ""
+    "MemberID" => "qa72oCvud/nRRFb92UdVzDZIu0SUT0v2kfQMkS0t+MiMbkZAw9D+O5BCCflZXTru",
+    "PageSize" => 10,
+    "CampaignType" => 0,
+    "MinClickPoint" => 0,
+    "MinCurrentPoint" => 0,
+    "ExistMiddleImage" => "",
+    "ExistRectangleImage" => "",
+    "IsPartner" => ""
 ));
 
 foreach($response->List as $item) {
-	$title = $item->ContentTitle;
-	$category = $item->CategoryName;
-	$memo = $item->ContentMemo;
-	$link = $item->Link;
-	$hash = get_hashed_text($link);
+    $title = $item->ContentTitle;
+    $category = $item->CategoryName;
+    $memo = $item->ContentMemo;
+    $link = $item->Link;
+    $hash = get_hashed_text($link);
 
-	$bind = array(
-		"hash" => $hash,
-		"datetime" => get_current_datetime(),
-		"title" => $title,
-		"category" => $category,
-		"memo" => $memo,
-		"link" => $link,
-		"normal_price" => 0,
-		"discount_price" => 0,
-		"network" => "tenping.kr"
-	);
-	$sql = get_bind_to_sql_insert($tablename, $bind, array(
-		"setignores" => array(
-			//array("eq", "hash", $hash)
-		)
-	));
-	exec_db_query($sql, $bind);
+    $bind = array(
+        "hash" => $hash,
+        "type" => "auto"
+        "datetime" => get_current_datetime(),
+        "title" => $title,
+        "category" => $category,
+        "memo" => $memo,
+        "link" => $link,
+        "normal_price" => 0,
+        "discount_price" => 0,
+        "network" => "tenping.kr"
+    );
+    $sql = get_bind_to_sql_insert($tablename, $bind, array(
+        "setignores" => array(
+            //array("eq", "hash", $hash)
+        )
+    ));
+    exec_db_query($sql, $bind);
 }
 
 
 // linkprice hotdeal
 $response = get_web_json("https://api.linkprice.com/ci/hotdeal/data/A100666607", "get");
 foreach($response as $item) {
-	$title = $item->product_name;
-	$category = $item->category;
-	$memo = $item->promotional_text;
-	$link = $item->click_url;
-	$hash = get_hashed_text($link);
+    $title = $item->product_name;
+    $category = $item->category;
+    $memo = $item->promotional_text;
+    $link = $item->click_url;
+    $hash = get_hashed_text($link);
 
-	$bind = array(
-		"hash" => $hash,
-		"datetime" => get_current_datetime(),
-		"title" => $title,
-		"category" => $category,
-		"memo" => $memo,
-		"link" => $link,
-		"normal_price" => $item->normal_price,
-		"discount_price" => $item->discount_price,
-		"network" => "api.linkprice.com"
-	);
-	$sql = get_bind_to_sql_insert($tablename, $bind, array(
-		"setkeys" => array("hash")
-	));
-	exec_db_query($sql, $bind);
+    $bind = array(
+        "hash" => $hash,
+        "type" => "auto"
+        "datetime" => get_current_datetime(),
+        "title" => $title,
+        "category" => $category,
+        "memo" => $memo,
+        "link" => $link,
+        "normal_price" => $item->normal_price,
+        "discount_price" => $item->discount_price,
+        "network" => "api.linkprice.com"
+    );
+    $sql = get_bind_to_sql_insert($tablename, $bind, array(
+        "setkeys" => array("hash")
+    ));
+    exec_db_query($sql, $bind);
 }
 
 // show list
@@ -105,11 +108,11 @@ $data = array();
 
 $bind = false;
 $sql = get_bind_to_sql_select($tablename, $bind, array(
-	"setorders" => array(
-		array("desc", "datetime")
-	),
-	"setlimit" => $limit,
-	"setpage" => 1
+    "setorders" => array(
+        array("desc", "datetime")
+    ),
+    "setlimit" => $limit,
+    "setpage" => 1
 ));
 $rows = exec_db_fetch_all($sql, $bind);
 $data['data'] = $rows;
